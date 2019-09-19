a688fbdb776e4bdfd49ed8430cd9f92049910890
Johnathon Lubaway <NeoJax@gmail.com>
2019-09-19 16:44:53 -0400

    Finished the project

---
+++ Fun_Places_To_Go/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fun_Places_To_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
